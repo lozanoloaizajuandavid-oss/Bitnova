3d36a4350040fd9e45eb5841a1333a9371628198
lozanoloaizajuandavid-oss <lozanoloaizajuandavid@gmail.com>
2025-12-16 15:43:53 -0500

    Create Servicios.php

---
+++ restaurante/Servicios.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina_blog</title>
+    <link rel="stylesheet" href="CSS/normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Emblema+One&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../pagina_cafe/CSS/style.css">
+</head>
+<body>
+<!--encabezado de pagina -->
+<header class="header">
+    <div class="color_header" style="background-color: #23310180; width: 100%; height: 100%;">
+    
+        <nav class="contenedor">
+            <div class="barra" style="padding-top: 4rem;">
+                <a href="index.html" class="logo">
+                    <h1 class="logo__nombre">
+                        Reservas Y <br>
+                        <span class="logo__bold">
+                            Menús
+                        </span>
+                    </h1>
+                </a>
+
+                <nav class="navegacion">
+                   <a href="Servicios.php" class="navegacion__enlace">Servicios</a>
+                </nav>
+            </div>
+        </nav>
+    
+        <div class="header__texto">
+            <h2 class="no-margin">Mundial del Sazón</h2>
+            <p class="no-margin frase">¿Antojo de algo delicioso? Ven a nuestro restaurante y prueba nuestros platos exclusivos. ¡La comida que te mereces está aquí!</p>
+        </div>
+    </div>
+</header>
+<!-- termina encabezado de pagina -->
+
+<!--contenido principal de la pagina-->
+<main class="contenedor">
+    <h2 class="centrar-texto">Servicios a ofrecer:</h2>
+
+    <div class="sobre_mi" style="display: grid; grid-template-columns: repeat(2, 1fr); column-gap: 2rem;">
+        <div class="sobre_mi__img">
+            <img src="https://image.freepik.com/vector-gratis/conjunto-iconos-servicios-restaurante_1284-6899.jpg" alt="imagen yo">
+            <img src="https://assets-global.website-files.com/6423e8cf97045d0ac6e9ffb9/64e486ac638f8577a303bf41_servicio%20de%20mesa%20en%20restaurantes.jpg" alt="imagen yo">
+            <img src="http://www.cursosgastronomia.com.mx/wp-content/uploads/2014/05/caracteristicas-de-restaurante-exitoso.JPG" alt="imagen yo">
+            <img src="https://www.pulperiasaurora.com/media/pulperiasaurora/images/2020/11/26/2020112610564620176.jpg" alt="imagen yo">
+            <img src="https://i.pinimg.com/originals/9f/e7/c8/9fe7c8640806a5fb6529bccbca5602d1.jpg" alt="imagen yo">
+
+        </div>
+                                  
+        <div class="sobre_mi__texto">
+            <p>
+                <strong>Servicios que ofrece el restaurante:</strong>
+            </p>
+            <h3>Desayunos completos:</h3>
+            <ul>
+                <li>Desayunos tradicionales, americanos, saludables y a la carta.</li>
+                <li>Opciones con café, jugos naturales, y variedad de panes y acompañamientos.</li>
+            </ul>
+
+            <h3>Almuerzos:</h3>
+            <ul>
+                <li>Menú del día con opciones de entrada, plato fuerte y postre.</li>
+                <li>Platos a la carta con opciones internacionales y locales.</li>
+                <li>Especialidades de la casa, incluyendo carnes, pescados, pastas y opciones vegetarianas.</li>
+            </ul>
+
+            <h3>Cenas:</h3>
+            <ul>
+                <li>Menú especial para cenas románticas, familiares o de negocios.</li>
+                <li>Platos gourmet y opciones vegetarianas y veganas.</li>
+                <li>Combinaciones exclusivas de vinos y platos gourmet.</li>
+            </ul>
+
+            <h3>Postre de cortesía:</h3>
+            <ul>
+                <li>Los clientes que reserven una mesa disfrutan de un postre gratuito.</li>
+                <li>Variedad de postres caseros y opciones de temporada.</li>
+            </ul>
+
+            <h3>Reservas de mesas:</h3>
+            <ul>
+                <li>Reservas para grupos pequeños o grandes.</li>
+                <li>Espacios privados para celebraciones especiales, reuniones de trabajo o eventos.</li>
+            </ul>
+
+            <h3>Decoración personalizada:</h3>
+            <ul>
+                <li>Decoración totalmente personalizada para cualquier tipo de evento.</li>
+                <li>Opciones de decoración temática, para bodas, cumpleaños, aniversarios, etc.</li>
+            </ul>
+
+            <h3>Atención especial:</h3>
+            <ul>
+                <li>Servicio al cliente excepcional, con un equipo preparado para atender peticiones especiales.</li>
+                <li>Menú personalizado según necesidades dietéticas o preferencias particulares.</li>
+            </ul>
+
+            <h3>Eventos especiales:</h3>
+            <ul>
+                <li>Organización de eventos privados como cenas temáticas, celebraciones y festividades.</li>
+                <li>Actividades como música en vivo o shows para grupos grandes.</li>
+            </ul>
+        </div>
+    </div>
+
+</main>
+<!--fin de contenido principal-->
+
+<footer class="footer">
+    <div class="contenedor">
+        <div class="barra" style="padding-top: 4rem;">
+            <a href="index.html" class="logo">
+                <h1 class="logo__nombre">
+                    Reservas y
+                    <span class="logo__bold">
+                        Menus
+                    </span>
+                </h1>
+            </a>
+
+            <nav class="navegacion">
+                <a href="Servicios.php" class="navegacion__enlace">Servicios</a>
+            </nav>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
